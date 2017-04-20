718042804ad2420e732706053aeddb588ac5909e
Hannah <hmt17@pitt.edu>
2017-04-19 22:40:43 -0400

    done

--- assignment4.js
+++ assignment4.js
@@ -1,17 +1,64 @@
-// The anonymous function below will fire on page load
-
-// Some things to consider
-// $.ajax(); to make your requests a little easier. Or the vanilla js way, it's up to you.
-// $.on(); for event handling
-// Remember, selecting elements in jQuery is like selecting them in CSS
-// You'll probably have to manipulate some strings
-// some jQuery functions to help display results
-// $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
-// Add content from requests with something like
-// $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
-
+//Hannah Tarzian
 (function() {
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
+	var global_input = $(".flexsearch-input").after("<ul class='flexsearch-results'></ul>");
+ 	// results/messages container
+ 	var glob_output = $(".flexsearch-results");
+ 	
+ 	// empty array
+ 	var words = [];
+ 
+ 	//parse words
+ 	$.when(
+ 		$.getJSON( 'http://www.mattbowytz.com/simple_api.json?data=programming', function(response){
+ 			$.each( response, function(key, val) {
+ 				console.log(val);
+ 				if(val !== 10 && val !== 200 && val.length > 2){
+ 					words =  words.concat(val);
+ 				}
+ 			});
+ 		}),
+ 
+ 		$.getJSON( 'http://www.mattbowytz.com/simple_api.json?data=interests', function(response){
+ 			$.each( response, function(key, val) {
+ 				if(val !== 9 && val !== 200 && val.length > 2){
+ 					words =  words.concat(val);
+ 				}
+ 			});
+ 		})
+  
+ 	).then(function() {
+ 		//clear and show all results
+ 		for(var i = 0; i < words.length; i++) {
+ 			glob_output.append("<li class='flexsearch-result'>" + words[i] + "</li>");
+ 		}
+ 
+ 		// autocomplete
+ 		global_input.on("keyup", function(event){
+ 
+ 			var input = global_input.val().toLowerCase();
+ 			var output = [];
+ 
+ 			for(var i  = 0; i < words.length; i++) {
+ 				// if substring is part of word, print
+ 				var tmp = words[i].toLowerCase();
+ 				
+ 				if(tmp.includes(input)) {
+ 					output.push(words[i]);
+ 				}
+ 			}
+ 
+ 			//clear results
+ 			glob_output.html("");
+ 			
+			//show output
+ 			if(output.length) {
+ 				for(i = 0; i < output.length; i++) {
+ 					glob_output.append("<li class='flexsearch-result'>" + output[i] + "</a></li>");
+ 				}
+ 			}
+ 		});
+ 	});
+  
 })();
